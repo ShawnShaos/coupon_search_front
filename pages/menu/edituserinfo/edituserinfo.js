08a318b408c18b1a3626b344b2095dc8d0fb706f
ly <958893303@qq.com>
2019-10-22 21:21:00 +0800

    初始化代码

---
+++ pages/menu/edituserinfo/edituserinfo.js
@@ -0,0 +1,19 @@
+Page({
+  data: {
+    TabCur: 0,
+    scrollLeft: 0,
+    TabListCur: 0
+  },
+  tabSelect(e) {
+    this.setData({
+      TabCur: e.currentTarget.dataset.id,
+      scrollLeft: (e.currentTarget.dataset.id - 1) * 60
+    })
+  },
+  tabListSelect(e) {
+    this.setData({
+      TabListCur: e.currentTarget.dataset.id,
+      scrollLeft: (e.currentTarget.dataset.id - 1) * 60
+    })
+  },
+})
